0e4d179d28490e2ce06ff62502ddc3408023f2ea
Zach Ewert <zachanima@gmail.com>
2010-09-13 12:30:30 +0200

    Add suppliers new, create actions

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,5 +1,22 @@
 class SuppliersController < ActionController::Base
+  def index
+    render :layout => 'application'
+  end
+
   def show
     @supplier = Supplier.find(params[:id])
+    render :layout => 'application'
+  end
+
+  def new
+    @supplier = Supplier.new
+    render :layout => 'application'
+  end
+
+  def create
+    @supplier = Supplier.new(params[:supplier])
+    @supplier.business = true
+    @supplier.save
+    redirect_to @supplier
   end
 end
