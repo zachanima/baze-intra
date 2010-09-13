074b316b9ab9788862738363d0ea1d6f5738ea3c
Zach Ewert <zachanima@gmail.com>
2010-09-13 13:42:19 +0200

    Fix controller inheritance, html4 markup rendering

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,16 +1,13 @@
-class SuppliersController < ActionController::Base
+class SuppliersController < ApplicationController
   def index
-    render :layout => 'application'
   end
 
   def show
     @supplier = Supplier.find(params[:id])
-    render :layout => 'application'
   end
 
   def new
     @supplier = Supplier.new
-    render :layout => 'application'
   end
 
   def create
