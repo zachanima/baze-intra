d3a4c8648f2846546fe9a0aeb1efd9611e1b7a01
Zach Ewert <zachanima@gmail.com>
2010-09-15 07:34:38 +0200

    Add supplier form markup, edit action

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,4 +1,6 @@
 class SuppliersController < ApplicationController
+  # def index
+
   def show
     @supplier = Supplier.find(params[:id])
   end
@@ -14,6 +16,20 @@ class SuppliersController < ApplicationController
       redirect_to @supplier
     else
       render :action => :new
+    end
+  end
+
+  def edit
+    @supplier = Supplier.find(params[:id])
+  end
+
+  def update
+    @supplier = Supplier.find(params[:id])
+
+    if @supplier.update_attributes(params[:supplier])
+      redirect_to @supplier
+    else
+      render :action => :edit
     end
   end
 end
