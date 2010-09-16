7de772099edc16e0230094f0ae6712cc3b550a29
Zach Ewert <zachanima@gmail.com>
2010-09-16 08:27:47 +0200

    Validate presence of name, value of kind

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,7 @@
 class Supplier < ActiveRecord::Base
+  validates_presence_of :name
+  validates_inclusion_of :kind, :in => ['business', 'shop']
+
   scope :business, where(:kind => 'business')
   scope :shop, where(:kind => 'shop')
 end
