51f01e72fe0c2ef38e737888c78cbab9168a14f2
Zach Ewert <zachanima@gmail.com>
2010-09-17 07:33:48 +0200

    Add orders update method to suppliers controller

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,8 +1,11 @@
 class SuppliersController < ApplicationController
+  before_filter :find_supplier, :only => [:show, :edit, :update, :orders_update]
+
   # def index
 
   def show
-    @supplier = Supplier.find(params[:id])
+    @order = Order.new
+    @orders = @supplier.orders
   end
 
   def new
@@ -11,7 +14,6 @@ class SuppliersController < ApplicationController
 
   def create
     @supplier = Supplier.new(params[:supplier])
-
     if @supplier.save
       redirect_to @supplier
     else
@@ -19,17 +21,31 @@ class SuppliersController < ApplicationController
     end
   end
 
-  def edit
-    @supplier = Supplier.find(params[:id])
-  end
+  # def edit
 
   def update
-    @supplier = Supplier.find(params[:id])
-
     if @supplier.update_attributes(params[:supplier])
       redirect_to @supplier
     else
       render :action => :edit
     end
+  end
+
+  def orders_update
+    if params[:create]
+      order_create
+    else
+      render :text => 'Did you say something?'
+    end
+    redirect_to @supplier
+  end
+
+  private
+  def find_supplier
+    @supplier = Supplier.find(params[:id])
+  end
+
+  def order_create
+    @supplier.orders.build(params[:order]).save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Intra::Application.routes.draw do
   resources :suppliers do
+    post :orders_update, :on => :member
     resources :orders
   end
 
