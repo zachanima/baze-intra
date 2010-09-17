8ff52c19cc556a3256bd9281d4e735ac167e0256
Zach Ewert <zachanima@gmail.com>
2010-09-17 10:49:22 +0200

    Implement order update method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,18 @@
 class OrdersController < ApplicationController
   before_filter :find_supplier
+  before_filter :find_order
 
   def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order.update_attributes(params[:order])
+    redirect_to @supplier
+  end
+    
+  private
+  def find_order
     @order = Order.find(params[:id])
   end
 end
