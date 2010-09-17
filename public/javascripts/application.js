db1782a094391052569a65e4c65ea2a746962e43
Zach Ewert <zachanima@gmail.com>
2010-09-17 11:20:35 +0200

    Enable, disable submit buttons on checkbox change

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,4 +6,8 @@ $(function() {
   $('input[name=ordered_by]').click(function() {
     $(this).attr('value', prompt('Hvem er du?'));
   });
+
+  $('table :checkbox').change(function() {
+    $('div#actions input[value]').attr('disabled', $('tbody :checked').length == 0);
+  });
 });
