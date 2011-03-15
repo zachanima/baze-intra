e81f6d1ba0adc56e7c45583c00bf88c4f382c5a3
Zach Ewert <zachanima@gmail.com>
2011-03-15 10:32:23 +0100

    Make find orders in suppliers controller dynamic

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -41,7 +41,7 @@ class SuppliersController < ApplicationController
   end
 
   def find_orders
-    @orders = Order.find(params[:order_ids]) unless params[:order_action] == 'create'
+    @orders = Order.find_all_by_id(params[:order_ids])
   end
 
   def orders_create
